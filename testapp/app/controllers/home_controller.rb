1b9e6702fd540132050bbdd2aced31584ad0259e
Truong Dung <truongdung@developers-iMac.local>
2015-08-27 11:42:30 +0700

    Add multi view

--- testapp/app/controllers/home_controller.rb
+++ testapp/app/controllers/home_controller.rb
@@ -1,11 +1,17 @@
 class HomeController < ApplicationController
   def index
     @data = {:name => 'Pete'}
-    @content = get_html(@data)
+    @content = get_html("HomeIndex", @data)
+    render :template => 'home/index'
   end
 
+  def about
+    @data = {}
+    @content = get_html("HomeAbout", @data)
+    render :template => 'home/index'
+  end
   private
-  def get_html(data)
-    RestClient.get 'http://localhost:3000/', :params => {:module => '../jsapp/src/App'.to_json, :props => data.to_json}
+  def get_html(component, data)
+    RestClient.get 'http://localhost:3000/', :params => {:module => component.to_json, :props => data.to_json}
   end
 end
--- testapp/config/initializers/assets.rb
+++ testapp/config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( bundle.js )
+Rails.application.config.assets.precompile += %w( home_index.bundle.js home_about.bundle.js )
